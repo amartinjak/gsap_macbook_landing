d7a5ea677995b7bae09af23fb7a4c8a0781b2559
amartinjak <amtinjak@gmail.com>
2026-02-14 19:02:25 +0100

    debugs

--- src/components/three/StudioLights.jsx
+++ src/components/three/StudioLights.jsx
@@ -1,41 +1,41 @@
-import {Environment} from "@react-three/drei";
+import {Environment, Lightformer} from "@react-three/drei";
 
 const StudioLights = () => {
     return (
         <group name="lights">
             <Environment resolution={256}>
                 <group>
-                    <lightformer
-                       form="rect"
-                       intensity={10}
-                       position={[-10, 5, -5]}
-                       scale={10}
-                       rotation-y={Math.PI / 2}
-                    />
-                    <lightformer
+                    <Lightformer
                         form="rect"
                         intensity={10}
-                        position={[10, 1, 1]}
+                        position={[-10, 5, -5]}
+                        scale={10}
+                        rotation-y={Math.PI / 2}
+                    />
+                    <Lightformer
+                        form="rect"
+                        intensity={10}
+                        position={[10, 0, 1]}
                         scale={10}
                         rotation-y={Math.PI / 2}
                     />
                 </group>
             </Environment>
-            <spotlight
+            <spotLight
                 position={[-2, 10, 5]}
-               angle={-.15}
-               decay={0}
-               intensity={Math.PI * 0.2}
-            />
-            <spotlight
-                position={[0, -25, 10]}
-                angle={-.15}
+                angle={0.15}
                 decay={0}
                 intensity={Math.PI * 0.2}
             />
-            <spotlight
+            <spotLight
+                position={[0, -25, 10]}
+                angle={0.15}
+                decay={0}
+                intensity={Math.PI * 0.2}
+            />
+            <spotLight
                 position={[0, 15, 5]}
-                angle={-.15}
+                angle={0.15}
                 decay={0.1}
                 intensity={Math.PI * 1}
             />
